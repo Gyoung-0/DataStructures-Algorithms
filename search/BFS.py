cec87be39aa7b0c8aec39746f63b115a10ca5fbf
Gyoung_0 <hkhj77777@gmail.com>
2025-04-05 18:08:42 +0900

    Deque 구현

---
+++ search/BFS.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ')
+
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
